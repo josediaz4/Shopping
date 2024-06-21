fd8c906ae5833c7ca14a8cf65438a098101e24dd
josediaz4 <diaz.jose.mnuel.04@gmail.com>
2024-06-21 18:46:52 -0300

    Finish creation DataContext, migrations and controller Countries

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no deber superar los {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
