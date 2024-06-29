8231eec0b2d79bdee902dcbd0e75648eceaa0aa4
josediaz4 <diaz.jose.mnuel.04@gmail.com>
2024-06-29 17:50:13 -0300

    UserHelper, IUserHelper and admin user creation

--- Shopping/Shopping/Data/SeedDb.cs
+++ Shopping/Shopping/Data/SeedDb.cs
@@ -1,4 +1,6 @@
 ﻿using Shopping.Data.Entities;
+using Shopping.Enum;
+using Shopping.Helpers;
 
 namespace Shopping.Data
 {
@@ -6,17 +8,59 @@ namespace Shopping.Data
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             _context = context;
-
+            _userHelper = userHelper;
         }
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckCategoriesAsync();
             await CheckCountriesAsync();
+            await CheckRolesAsync();
+            await CheckUserAsync("1010", "Manu", "Diaz", "manu@yopmail.com", "3815556397", "Av Calchaqui 159", UserType.Admin);
+        }
+
+        private async Task<User> CheckUserAsync(
+             string document,
+             string firstName,
+             string lastName,
+             string email,
+             string phone,
+             string address,
+             UserType userType)
+        {
+            User user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    Document = document,
+                    City = _context.Cities.FirstOrDefault(),
+                    UserType = userType,
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+
+            return user;
+
+        }
+
+        private async Task CheckRolesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
         }
 
         private async Task CheckCountriesAsync()
--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Shopping.Data;
+using Shopping.Data.Entities;
+using Shopping.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,22 @@ builder.Services.AddDbContext<DataContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+//Configuramos el program para decirle que tenemos usuarios (Requisitos para crear usuarios)
+builder.Services.AddIdentity<User, IdentityRole>(cfg =>
+{
+    cfg.User.RequireUniqueEmail = true;
+    cfg.Password.RequireDigit = false;
+    cfg.Password.RequiredUniqueChars = 0;
+    cfg.Password.RequireLowercase = false;
+    cfg.Password.RequireNonAlphanumeric = false;
+    cfg.Password.RequireUppercase = false;
+}).AddEntityFrameworkStores<DataContext>();
+
 
 //Inyectamos la Clase SeeDb 
-builder.Services.AddTransient<SeedDb>(); 
+builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation(); //Esta linea se usa para no tener que estar lanzando el proyecto cada vez que se cambie alguna vista
 
 var app = builder.Build();
 
@@ -41,10 +56,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication(); //Con esta linea le decimos al program que usaremos autenticacion cada vez que larguemos
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
